55654c830935b37f2ed983f28047d83027facbcb
Erwan Vallée <erwanp.vallee@gmail.com>
2020-07-24 17:44:42 +0200

    maj deps

--- database/index.js
+++ database/index.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
-const env = require(`../environment/${ process.env.NODE_ENV }`);
+const env = require(`../environment/${process.env.NODE_ENV}`);
 
-mongoose.connect(env.dbUrl, { 
-  useNewUrlParser: true,
-  useCreateIndex: true
-}).then( () => console.log('connexion db ok !')).catch( err => console.log(err));
+mongoose
+  .connect(env.dbUrl, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('connexion db ok !'))
+  .catch((err) => console.log(err));
