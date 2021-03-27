0468a8dc8ad7e3cf5eb66292ba97a340c2532981
dymafr <contact@dyma.fr>
2021-03-27 14:50:48 +0100

    maj

--- config/session.config.js
+++ config/session.config.js
@@ -1,18 +1,20 @@
 const app = require('../app');
 const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
-const mongoose = require('mongoose');
+const MongoStore = require('connect-mongo');
+const { clientPromise } = require('../database');
 
-app.use(session({
-  secret: 'je suis un secret',
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    httpOnly: false,
-    maxAge: 1000 * 60 * 60 * 24 * 14
-  },
-  store: new MongoStore({
-    mongooseConnection: mongoose.connection,
-    ttl: 60 *60 * 24 * 14
+app.use(
+  session({
+    secret: 'je suis un secret',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: false,
+      maxAge: 1000 * 60 * 60 * 24 * 14,
+    },
+    store: MongoStore.create({
+      clientPromise: clientPromise.then((m) => m.connection.getClient()),
+      ttl: 60 * 60 * 24 * 14,
+    }),
   })
-}));
+);
--- database/index.js
+++ database/index.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
 const env = require(`../environment/${process.env.NODE_ENV}`);
 
-mongoose
+exports.clientPromise = mongoose
   .connect(env.dbUrl, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log('connexion db ok !'))
   .catch((err) => console.log(err));
