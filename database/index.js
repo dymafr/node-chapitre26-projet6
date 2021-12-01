21f467b71257bbf76e9892026c1df90020e8622b
dymafr <contact@dyma.fr>
2021-12-01 13:28:24 +0100

    maj

--- database/index.js
+++ database/index.js
@@ -4,7 +4,6 @@ const env = require(`../environment/${process.env.NODE_ENV}`);
 exports.clientPromise = mongoose
   .connect(env.dbUrl, {
     useNewUrlParser: true,
-    useCreateIndex: true,
     useUnifiedTopology: true,
   })
   .catch((err) => console.log(err));
